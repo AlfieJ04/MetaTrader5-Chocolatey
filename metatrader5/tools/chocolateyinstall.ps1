a96f8d4b3d6ac44f065a5dab21ed11ae572f9e0c
Ashlee Jones <ashlee.jones04@gmail.com>
2020-11-16 12:41:57 +0000

    Created choco package

--- metatrader5/tools/chocolateyinstall.ps1
+++ metatrader5/tools/chocolateyinstall.ps1
@@ -1,21 +1,15 @@
-﻿
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe?utm_source=www.metatrader5.com&utm_campaign=download'
+$url        = 'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-
-  softwareName  = 'MetaTrader5*'
-
-  checksum      = '4DB5C31334E88578C33F528E0A4A7C51'
-  checksumType  = 'md5'
-
-  silentArgs   = '/S'
-  validExitCodes= @(0)
+  softwareName  = 'MetaTrader5'
+  silentArgs   = '/auto'
+  validExitCodes= @(1)
 }
 
 Install-ChocolateyPackage @packageArgs

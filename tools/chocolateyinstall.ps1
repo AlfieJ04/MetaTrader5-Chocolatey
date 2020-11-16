6bc74f0ef70509dfacebdc4b53a50db81405815a
Ashlee Jones <ashlee.jones04@gmail.com>
2020-11-16 11:50:40 +0000

    Initial Commit
    Created Chocolatey package

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe?utm_source=www.metatrader5.com&utm_campaign=download'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'MetaTrader5*'
+
+  checksum      = '4DB5C31334E88578C33F528E0A4A7C51'
+  checksumType  = 'md5'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
